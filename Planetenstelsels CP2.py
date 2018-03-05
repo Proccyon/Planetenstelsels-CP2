bf694ab548fdeb79a0604b895f0724098879a763
Proccyon <36238733+Proccyon@users.noreply.github.com>
2018-03-05 20:01:26 +0100

    Opdracht 1 deels gemaakt

--- Planetenstelsels CP2.py
+++ Planetenstelsels CP2.py
@@ -1,2 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+Data = np.loadtxt("C:\Users\Eigenaar\Desktop\Uni\Planetenstelsels\CP2\Data.dat")
+Phase = Data [:,0]
+Flux = Data [:,1]
+
+plt.scatter(Phase,Flux,marker=".",s = 1,color = "black")
+plt.title("Relative flux of star WASP-203 VS phase of exoplanet WASP-203b")
+plt.xlabel("Phase (0 at transit)")
+plt.ylabel("Relative Flux")
+plt.xlim(np.amin(Phase),np.amax(Phase))
+plt.show()
