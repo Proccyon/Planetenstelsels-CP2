d9ef0039c63a2581d455a3bca10b4fa28ff66c9d
Proccyon <36238733+Proccyon@users.noreply.github.com>
2018-03-05 20:16:56 +0100

    1.1 en 1.2 afgemaakt
    Flux berekent rond phase = 0

--- Planetenstelsels CP2.py
+++ Planetenstelsels CP2.py
@@ -1,10 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#Load data
 Data = np.loadtxt("C:\Users\Eigenaar\Desktop\Uni\Planetenstelsels\CP2\Data.dat")
 Phase = Data [:,0]
 Flux = Data [:,1]
 
+#Calculate average around Phase = 0
+Delta_P = 0.005 
+TransitPhase = (Phase > -Delta_P) & (Phase < Delta_P)
+TransitFlux = Flux[TransitPhase]
+AverageFlux = np.average(TransitFlux)
+print "Average flux around Phase = 0: " + str(AverageFlux)
+
+
+#Scatter plot of flux of WASP-203
 plt.scatter(Phase,Flux,marker=".",s = 1,color = "black")
 plt.title("Relative flux of star WASP-203 VS phase of exoplanet WASP-203b")
 plt.xlabel("Phase (0 at transit)")
